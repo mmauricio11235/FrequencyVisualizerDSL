057b156a2149097af5432390846c8b3d42c01452
Mauricio Molina <mmauricio11235@gmail.com>
2014-12-13 00:33:15 +0000

    Cleaned up simple example of the DSL

--- FrequencyVisualizerDSL/src/TestVisualizers/TestVisualizer.java
+++ FrequencyVisualizerDSL/src/TestVisualizers/TestVisualizer.java
@@ -7,9 +7,15 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import FrequencyVisualizerDSL.*;
 import objectdraw.*;
 
+
+/**
+ * This is a simple example of using the Java Music Visualizer DSL
+ * 
+ * @author Mauricio Molina
+ *
+ */
 public class TestVisualizer extends FrequencyVisualizerDSL{
 
-	
 	public static void main(String[] args) throws LineUnavailableException, IOException, 
 	UnsupportedAudioFileException, InterruptedException {
 		
@@ -22,13 +28,17 @@ public class TestVisualizer extends FrequencyVisualizerDSL{
 		+ "97118d4d45c44a0a9361a1b2aba8370c.image!jpeg.288619.jpg.EDC-event-image.jpg");
 		
 		/** General Structure of the DSL is 
-		 Time(startTime, endTime);
-		 	Frequency(startTime,endTime);
-		 		
-		  
+		 Time(start, end);
+		 	Frequency(start,end);
+		 		Image(imageDefinition);
+		 			Effect(effectType);
 		 
+		 * start: value at which the given object starts controlling 
+		 * end: value at which the given object no long has control
 		 * 
 		 */
+		
+		//Initial Twinkling
 		Time(0,5);
 			Frequency(600,50);
 				Amplitude(0,100);	
@@ -36,6 +46,8 @@ public class TestVisualizer extends FrequencyVisualizerDSL{
 					Image(new FilledOval(500,50,10,10, canvas));
 						Effect("random");
 				}
+				
+		//Additional twinkling
 		Time(2.5,5);
 			Frequency(40,50);
 			Amplitude(0,100);	
@@ -52,48 +64,50 @@ public class TestVisualizer extends FrequencyVisualizerDSL{
 					Image(new FilledOval(500,50,20,20, canvas));
 						Effect("random");
 				}
-					
+		
+		//Frequency bars start animation. Each frequency definition represents a bar
 		Time(5.7,100);
-		Frequency(0,10);
-			Amplitude(0,50);
-				Image(new FilledRect(0,50,100,50, canvas));
-					//Maybe use Enum instead of a string? 
-					Effect("Bounce");
-				Image(new FilledRect(50,50,100,50, canvas));
-					Effect("Bounce");
-		Frequency(10,20);
-			Amplitude(0,100);
-				Image(new FilledRect(100,50,100,50, canvas));
-					Effect("Bounce");
-		Frequency(20,30);
-			Amplitude(0,100);
-				Image(new FilledOval(300,50,100,50, canvas));
-					Effect("Bounce");
-		Frequency(30,40);
-			Amplitude(0,100);	
-				Image(new FilledRect(400,50,100,50, canvas));
-					Effect("Bounce");
-		Frequency(40,50);
-			Amplitude(0,100);	
-				Image(new FilledRect(500,50,100,50, canvas));
-					Effect("bounce");
-		Frequency(50,60);
-			Amplitude(0,100);	
-				Image(new FilledRect(600,50,100,50, canvas));
-					Effect("Bounce");
-		Frequency(60,70);
-			Amplitude(0,100);	
-				Image(new FilledRect(700,50,100,50, canvas));
-					Effect("Bounce");
-		Frequency(60,70);
-			Amplitude(0,100);	
-				Image(new FilledRect(800,50,100,50, canvas));
-					Effect("Bounce");
-		Frequency(60,70);
-			Amplitude(0,100);	
-				Image(new FilledRect(900,50,100,50, canvas));
-					Effect("Bounce");
-					
+			Frequency(0,10);
+				Amplitude(0,50);
+					Image(new FilledRect(0,50,100,50, canvas));
+						//Maybe use Enum instead of a string? 
+						Effect("Bounce");
+					Image(new FilledRect(50,50,100,50, canvas));
+						Effect("Bounce");
+			Frequency(10,20);
+				Amplitude(0,100);
+					Image(new FilledRect(100,50,100,50, canvas));
+						Effect("Bounce");
+			Frequency(20,30);
+				Amplitude(0,100);
+					Image(new FilledOval(300,50,100,50, canvas));
+						Effect("Bounce");
+			Frequency(30,40);
+				Amplitude(0,100);	
+					Image(new FilledRect(400,50,100,50, canvas));
+						Effect("Bounce");
+			Frequency(40,50);
+				Amplitude(0,100);	
+					Image(new FilledRect(500,50,100,50, canvas));
+						Effect("bounce");
+			Frequency(50,60);
+				Amplitude(0,100);	
+					Image(new FilledRect(600,50,100,50, canvas));
+						Effect("Bounce");
+			Frequency(60,70);
+				Amplitude(0,100);	
+					Image(new FilledRect(700,50,100,50, canvas));
+						Effect("Bounce");
+			Frequency(60,70);
+				Amplitude(0,100);	
+					Image(new FilledRect(800,50,100,50, canvas));
+						Effect("Bounce");
+			Frequency(60,70);
+				Amplitude(0,100);	
+					Image(new FilledRect(900,50,100,50, canvas));
+						Effect("Bounce");
+				
+		//This is the required method that comes at the end of all the definitions. Will start the visualizer. 
 		VisualizerStart();
 			
 		
